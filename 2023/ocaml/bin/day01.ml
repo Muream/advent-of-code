29b1f33df802f519097f0df0541001eeea3050a9
Loïc Pinsard <muream@gmail.com>
2023-12-04 23:58:45 +0000

    solve 2023 day01 part2

--- 2023/ocaml/bin/day01.ml
+++ 2023/ocaml/bin/day01.ml
@@ -1,7 +1,5 @@
 open Core
 
-let pattern = Re2.create_exn "(one|two|three|four|five|six|seven|eight|nine)"
-
 let part1 lines =
   let numbers =
     List.map lines ~f:(fun line ->
@@ -16,32 +14,48 @@ let part1 lines =
   total
 ;;
 
-let numberify string =
-  Re2.rewrite_exn pattern ~template:"_\\1_" string
-  |> String.substr_replace_all ~pattern:"_one_" ~with_:"1"
-  |> String.substr_replace_all ~pattern:"_two_" ~with_:"2"
-  |> String.substr_replace_all ~pattern:"_three_" ~with_:"3"
-  |> String.substr_replace_all ~pattern:"_four_" ~with_:"4"
-  |> String.substr_replace_all ~pattern:"_five_" ~with_:"5"
-  |> String.substr_replace_all ~pattern:"_six_" ~with_:"6"
-  |> String.substr_replace_all ~pattern:"_seven_" ~with_:"7"
-  |> String.substr_replace_all ~pattern:"_eight_" ~with_:"8"
-  |> String.substr_replace_all ~pattern:"_nine_" ~with_:"9"
+(** Return whatever number this string starts with *)
+let get_number string =
+  match string with
+  | s when String.is_prefix s ~prefix:"one" -> Some "1"
+  | s when String.is_prefix s ~prefix:"two" -> Some "2"
+  | s when String.is_prefix s ~prefix:"three" -> Some "3"
+  | s when String.is_prefix s ~prefix:"four" -> Some "4"
+  | s when String.is_prefix s ~prefix:"five" -> Some "5"
+  | s when String.is_prefix s ~prefix:"six" -> Some "6"
+  | s when String.is_prefix s ~prefix:"seven" -> Some "7"
+  | s when String.is_prefix s ~prefix:"eight" -> Some "8"
+  | s when String.is_prefix s ~prefix:"nine" -> Some "9"
+  | s when String.is_prefix s ~prefix:"1" -> Some "1"
+  | s when String.is_prefix s ~prefix:"2" -> Some "2"
+  | s when String.is_prefix s ~prefix:"3" -> Some "3"
+  | s when String.is_prefix s ~prefix:"4" -> Some "4"
+  | s when String.is_prefix s ~prefix:"5" -> Some "5"
+  | s when String.is_prefix s ~prefix:"6" -> Some "6"
+  | s when String.is_prefix s ~prefix:"7" -> Some "7"
+  | s when String.is_prefix s ~prefix:"8" -> Some "8"
+  | s when String.is_prefix s ~prefix:"9" -> Some "9"
+  | _ -> None
+;;
+
+(** Lists all the digits in the string whether they're spelled out of written as a numbers *)
+let get_numbers string =
+  let rec aux string acc =
+    match string with
+    | "" -> acc
+    | str ->
+      (match get_number str with
+       | Some n -> aux (String.sub str ~pos:1 ~len:(String.length str - 1)) (n :: acc)
+       | None -> aux (String.sub str ~pos:1 ~len:(String.length str - 1)) acc)
+  in
+  List.rev (aux string [])
 ;;
 
 let part2 lines =
   let numbers =
     List.map lines ~f:(fun line ->
-      print_endline line;
-      let numberified = numberify line in
-      print_endline numberified;
-      let chars = String.to_list numberified in
-      let numbers = List.filter chars ~f:Char.is_digit in
-      let number =
-        String.of_char (List.hd_exn numbers) ^ String.of_char (List.last_exn numbers)
-      in
-      print_endline number;
-      print_endline "";
+      let numbers = get_numbers line in
+      let number = List.hd_exn numbers ^ List.last_exn numbers in
       Int.of_string number)
   in
   let total = List.reduce_exn numbers ~f:( + ) in
@@ -50,11 +64,20 @@ let part2 lines =
 
 let () =
   let part1_res_test = part1 (In_channel.read_lines "../input/day01-01-test.txt") in
-  Printf.printf "Day 01 - Part 01 - test: %i\n" part1_res_test;
+  Printf.printf "Day 01 - Part 01 - test: %i\n" part1_res_test
+;;
+
+let () =
   let part1_res = part1 (In_channel.read_lines "../input/day01-01.txt") in
-  Printf.printf "Day 01 - Part 01: %i\n" part1_res;
+  Printf.printf "Day 01 - Part 01: %i\n" part1_res
+;;
+
+let () =
   let part2_res_test = part2 (In_channel.read_lines "../input/day01-02-test.txt") in
-  Printf.printf "Day 01 - Part 02 - test: %i\n" part2_res_test;
+  Printf.printf "Day 01 - Part 02 - test: %i\n" part2_res_test
+;;
+
+let () =
   let part2_res = part2 (In_channel.read_lines "../input/day01-02.txt") in
   Printf.printf "Day 01 - Part 02: %i\n" part2_res
 ;;
