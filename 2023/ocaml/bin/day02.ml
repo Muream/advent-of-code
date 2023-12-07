9a9010e0b86ec262e61636c78c3bb1f1aa3e78bf
Lo??c Pinsard <muream@gmail.com>
2023-12-07 00:10:43 +0000

    solve 2023 day02 part 2

--- 2023/ocaml/bin/day02.ml
+++ 2023/ocaml/bin/day02.ml
@@ -6,8 +6,7 @@ let blue_pat = Re.Perl.compile_pat {|(\d+) blue|}
 let max_list lst = List.fold lst ~init:0 ~f:max
 
 let max_of_match matches =
-  List.map matches ~f:(fun m -> Re.Group.get m 1 |> int_of_string)
-  |> max_list
+  List.map matches ~f:(fun m -> Re.Group.get m 1 |> int_of_string) |> max_list
 ;;
 
 let part1 lines =
@@ -19,12 +18,31 @@ let part1 lines =
   |> List.fold ~init:0 ~f:( + )
 ;;
 
+let part2 lines =
+  List.map lines ~f:(fun line ->
+    let max_red = Re.all red_pat line |> max_of_match in
+    let max_green = Re.all green_pat line |> max_of_match in
+    let max_blue = Re.all blue_pat line |> max_of_match in
+    max_red * max_green * max_blue)
+  |> List.fold ~init:0 ~f:( + )
+;;
+
 let () =
-  let part1_res_test = part1 (In_channel.read_lines "../input/day02-01-test.txt") in
+  let part1_res_test = part1 (In_channel.read_lines "../input/day02-test.txt") in
   Printf.printf "Day 02 - Part 01 - test: %i\n" part1_res_test
 ;;
 
 let () =
-  let part1_res = part1 (In_channel.read_lines "../input/day02-01.txt") in
+  let part1_res = part1 (In_channel.read_lines "../input/day02.txt") in
   Printf.printf "Day 02 - Part 01: %i\n" part1_res
+;;
+
+let () =
+  let part2_res_test = part2 (In_channel.read_lines "../input/day02-test.txt") in
+  Printf.printf "Day 02 - Part 02 - test: %i\n" part2_res_test
+;;
+
+let () =
+  let part2_res = part2 (In_channel.read_lines "../input/day02.txt") in
+  Printf.printf "Day 02 - Part 02: %i\n" part2_res
 ;;
